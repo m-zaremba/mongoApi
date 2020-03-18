d350a8ad5a25b39adece7cac24534683d22ca7bd
m-zaremba <zaremba_michal@wp.pl>
2020-03-18 10:46:34 +0100

    mongoDB connection

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,22 @@
+const Post = require('../models/post.js');
+
 exports.getPosts = (req, res) => {
   res.json({
     posts: [{ title: 'First post' }, { title: 'Second post' }],
+  });
+};
+
+exports.createPost = (req, res) => {
+  const post = new Post(req.body);
+
+  post.save((err, result) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      post: result,
+    });
   });
 };
--- server.js
+++ server.js
@@ -1,15 +1,33 @@
+/* eslint-disable no-console */
+/* eslint-disable promise/catch-or-return */
 const express = require('express');
 
 const app = express();
+
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const postRoutes = require('./routes/post.js');
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+mongoose.connection.on('error', (err) => {
+  console.log(`MongoDB connection error: ${err}`);
+});
 
 app.use(morgan('dev'));
-
+app.use(express.json());
 app.use('/', postRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server listening to port ${port}`);
 });
